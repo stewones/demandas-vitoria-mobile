564548a6a65946b8c4d7478377ff90ef5043d56f
osamuca <sfverneck@gmail.com>
2018-01-22 13:21:18 -0200

    feat(app): versão de base para desenvolvimento

---
+++ src/pages/about/about.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  demandasForm: FormGroup;
+  
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder) {
+
+    this.demandasForm = this.formBuilder.group ({
+      nome: [''],
+      telefone: [''],
+      endereco: [''],
+      bairro: [''],
+      demanda: ['']
+    })
+  }
+}
